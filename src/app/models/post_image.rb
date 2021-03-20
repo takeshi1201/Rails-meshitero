e784b35f34b461e74ad4429bc4260c08de79fa39
takeshi1201 <kodama19961201@gmail.com>
2021-03-20 17:09:54 +0900

    Add:簡易的ないいね機能追加

--- src/app/models/post_image.rb
+++ src/app/models/post_image.rb
@@ -2,6 +2,10 @@ class PostImage < ApplicationRecord
   
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   attachment :image # ここを追加（_idは含めません）
 end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_024019) do
+ActiveRecord::Schema.define(version: 2021_03_20_072653) do
+
+  create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "post_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "comment"
